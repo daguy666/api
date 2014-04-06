6dfe5a78a736fca8ef0116ed7cb16feb0c0cd5ed
daguy666 <jpistonejr@gmail.com>
2014-04-06 10:43:34 -0400

    grammer fix

--- haveibeenowned.py
+++ haveibeenowned.py
@@ -2,28 +2,29 @@
 from pprint import pprint
 import json
 import urllib2
-""" This is is using the haveibeenpwned.com api to search for owned accounts. 
+
+""" This is using the haveibeenpwned.com api to search for owned accounts. 
 You are searching for email accounts on the following compromised websites;
      accounts           sites
      --------           -----
-    152,445,165     Adobe accounts 
-    4,609,615       Snapchat accounts 
-    1,247,574       Gawker accounts 
-    1,057,819       Forbes accounts 
-    859,777         Stratfor accounts 
-    530,270         Battlefield Heroes accounts 
-    453,427         Yahoo accounts 
-    158,093         Boxee accounts 
-    148,366         WPT Amateur Poker League accounts 
-    56,021          Vodafone accounts 
-    55,622          Spirol accounts 
-    38,108          Pixel Federation accounts 
-    37,784          Muslim Directory accounts 
-    37,103          Sony accounts 
-    28,641          hemmelig.com accounts 
-    20,902          Bell accounts 
-    3,200           UN Internet Governance Forum accounts 
-    2,239           Tesco accounts 
+    152,445,165     Adobe accounts
+    4,609,615       Snapchat accounts
+    1,247,574       Gawker accounts
+    1,057,819       Forbes accounts
+    859,777         Stratfor accounts
+    530,270         Battlefield Heroes accounts
+    453,427         Yahoo accounts
+    158,093         Boxee accounts
+    148,366         WPT Amateur Poker League accounts
+    56,021          Vodafone accounts
+    55,622          Spirol accounts
+    38,108          Pixel Federation accounts
+    37,784          Muslim Directory accounts
+    37,103          Sony accounts
+    28,641          hemmelig.com accounts
+    20,902          Bell accounts
+    3,200           UN Internet Governance Forum accounts
+    2,239           Tesco accounts
 """
 email = raw_input("Please enter the email account you would like to check: ")
 www = "https://haveibeenpwned.com/api/v2/breachedaccount/%s" % (email)
