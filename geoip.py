6b3952d51f6a4072122f292b344990ef82c251ed
daguy666 <jpistonejr@gmail.com>
2014-04-08 17:37:39 -0400

    cleaned up print output

--- geoip.py
+++ geoip.py
@@ -11,13 +11,9 @@ result = urllib2.urlopen(request)
 my_json = json.load(result)
 #fixed_json = pprint(my_json)
 
-print "You are in the country of: "
+print "The IP %s is located the country of: " % ip
 print my_json['country_name']
-print "Your area code is: "
-print my_json['area_code']
-print "You are near the city of: "
+print "Your IP is near the city of: "
 print my_json['city']
-print "You are in the state of: "
+print "The IP is in the state of: "
 print my_json['region_name']
-print "Your zip code is: "
-print my_json['zipcode']
