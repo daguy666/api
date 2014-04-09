37184a30e9b3939e43c3d692c1f40ebcaeda2bc0
daguy666 <jpistonejr@gmail.com>
2014-04-09 10:40:18 -0400

    offline country geo ip lookup

---
+++ offline_country_geoip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Offline geoip lookup 
+
+import pygeoip
+
+gi = pygeoip.GeoIP('/usr/local/geo/GeoIP.dat')
+url = raw_input("Wich url would you like to locate: ")
+print gi.country_code_by_name(url)
+
+
+
