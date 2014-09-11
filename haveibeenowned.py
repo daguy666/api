3fa80dd5ea71d1be0b275142a747e3496a32f2e5
Joe Pistone <jpistone@etsy.com>
2014-09-11 08:54:35 -0400

    updated website list for ownage

--- haveibeenowned.py
+++ haveibeenowned.py
@@ -3,34 +3,41 @@ from pprint import pprint
 import json
 import urllib2
 
-
-sites = """ This is using the haveibeenpwned.com api to search for owned accounts.
-You are searching for email accounts on the following compromised websites
-    accounts          sites
-    ----------       --------
-    152,445,165		Adobe accounts
-    4,609,615		Snapchat accounts
-    1,247,574		Gawker accounts
-    1,057,819		Forbes accounts
-    859,777			Stratfor accounts
-    530,270			Battlefield Heroes accounts
-    453,427			Yahoo accounts
-    91,540			hackforums.net accounts
-    158,093			Boxee accounts
-    148,366			WPT Amateur Poker League accounts
-    56,021			Vodafone accounts
-    55,622			Spirol accounts
-    38,108			Pixel Federation accounts
-    37,784			Muslim Directory accounts
-    37,103			Sony accounts
-    6,789			BigMoneyJobs accounts
-    35,368			Fridae accounts
-    28,641			hemmelig.com accounts
-    26,596			Business Acumen Magazine accounts
-    20,902			Bell accounts
-    3,200			UN Internet Governance Forum accounts
-    2,239			Tesco accounts
+sites = """
+31 pwned websites  168,443,583  pwned accounts
+152,445,165 Adobe accounts 
+4,789,599   Bitcoin Security Forum Gmail Dump accounts 
+4,609,615   Snapchat accounts 
+1,247,574   Gawker accounts 
+1,057,819   Forbes accounts 
+859,777 Stratfor accounts 
+855,249 Manga Traders accounts 
+530,270 Battlefield Heroes accounts 
+453,427 Yahoo accounts 
+227,746 Cannabis.com accounts 
+202,683 Win7Vista Forum accounts 
+191,540 hackforums.net accounts 
+158,093 Boxee accounts 
+148,366 WPT Amateur Poker League accounts 
+116,465 Pokemon Creed accounts 
+104,097 Insanelyi accounts 
+56,021  Vodafone accounts 
+55,622  Spirol accounts 
+45,018  Lounge Board accounts 
+38,108  Pixel Federation accounts 
+37,784  Muslim Directory accounts 
+37,103  Sony accounts 
+36,789  BigMoneyJobs accounts 
+35,368  Fridae accounts 
+28,641  hemmelig.com accounts 
+26,596  Business Acumen Magazine accounts 
+20,902  Bell accounts 
+16,919  Verified accounts 
+5,788   Astropid accounts 
+3,200   UN Internet Governance Forum accounts 
+2,239   Tesco accounts 
 """
+
 email = raw_input("Please enter the email account you would like to check: ")  # Which email address to check
 www = "https://haveibeenpwned.com/api/v2/breachedaccount/%s" % email
 
